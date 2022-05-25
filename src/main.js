d63f8e0a6b7cfef562551d01d0a02963b5679cf6
Tito Reinhart <TKral@pdx.edu>
2022-05-25 15:35:13 -0700

    added photo

---
+++ src/main.js
@@ -0,0 +1,4 @@
+const header = document.createElement("h1");
+const img = document.createElement("img");
+header.innerText = "Tito's Website";
+document.body.appendChild(header).setAttribute("style", "text-align:center");
