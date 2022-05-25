5ff30a3923dfda9d3283d8022ed010bc874bc8c2
Tito Reinhart <TKral@pdx.edu>
2022-05-25 15:35:23 -0700

    adding functionality

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 const header = document.createElement("h1");
 const img = document.createElement("img");
+
 header.innerText = "Tito's Website";
+img.src = "../tito.jpg";
+img.height = 500;
+img.width = 350;
+img.style.display = "center";
+
 document.body.appendChild(header).setAttribute("style", "text-align:center");
+document.body.appendChild(img).setAttribute("alt", "portrait of tito");
