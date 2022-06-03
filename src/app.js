1cd6d89bca8bf45b79733b540a9ab9bb731afcea
Tito Reinhart <TKral@pdx.edu>
2022-06-03 16:54:57 -0700

    add message

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const move = document.querySelector("#button");
-
+const msg = "No Post Method Setup Yet";
 move.addEventListener("click", () => {
+    console.log(msg);
     window.scrollTo({
         top: 0,
         left: 0,
         behavior: "smooth",
     });
-    console.log("No Post Method Setup Yet");
 });
