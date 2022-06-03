5a9c4fcc083d9510d82284831240642dea50ffeb
Tito Reinhart <TKral@pdx.edu>
2022-06-03 16:58:13 -0700

    cant console log

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const move = document.querySelector("#button");
-const msg = "No Post Method Setup Yet";
+
 move.addEventListener("click", () => {
-    console.log(msg);
+    const msg = "No Post Method Setup Yet";
     window.scrollTo({
         top: 0,
         left: 0,
         behavior: "smooth",
     });
+    console.log(msg);
 });
