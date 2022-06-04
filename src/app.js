32a08a575ecf8a2f96edfd029567a619b16a9989
Tito Reinhart <TKral@pdx.edu>
2022-06-03 17:29:37 -0700

    console log

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const moveTop = document.querySelector("#button");
 
 moveTop.addEventListener("click", () => {
+    const msg = "email not implemented";
     window.scrollTo({
         top: 0,
         left: 0,
     });
+    console.log(msg);
 });
