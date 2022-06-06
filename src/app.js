3318fce6ed7b819be9b6a30d8f8b118ab69be81f
Tito Reinhart <TKral@pdx.edu>
2022-06-06 09:16:50 -0700

    update scroll

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 const moveTop = document.querySelector("#button");
 
 moveTop.addEventListener("click", () => {
-    const msg = "email not implemented";
     window.scrollTo({
         top: 0,
         left: 0,
     });
-    console.log(msg);
 });
