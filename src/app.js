b0dea6c195cb10ff24468a273eefbbe8f448dc39
Tito Reinhart <tkral@pdx.edu>
2022-06-06 14:02:15 -0700

    console log not working

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ $(document).ready(function () {
   const moveTop = document.querySelector("#main-content");
 
   moveTop.addEventListener("click", () => {
+    const msg = "Button Clicked";
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: "smooth",
     });
+    console.log(msg);
   });
 });
